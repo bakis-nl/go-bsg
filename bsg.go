f3429632e5dc74f5bbb4427b152535495ae036e8
bakis-nl <baki.sevgul@gmail.com>
2022-02-01 18:15:33 +0100

    xm output

--- bsg.go
+++ bsg.go
@@ -1,14 +1,40 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+	"github.com/gin-gonic/gin"
+)
 
-func main() {
+func mainBlock() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "baki was here !...",
+			"message": "hello world",
 		})
 	})
+	router.Run(":8061")
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", IndexHandlerXML)
 
 	router.Run(":8060")
+}
+
+func IndexHandlerJSON(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "baki was here !...",
+	})
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
+}
+
+func IndexHandlerXML(c *gin.Context) {
+	c.XML(200, Person{FirstName: "Baki",
+		LastName: "Sevgul"})
 }
